ea2ad323340044155a54de3242df2f0b27753698
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2018-10-11 12:10:30 +0200

    Replace _c_ with _p_ for Prosjekter

--- oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
+++ oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
@@ -1,7 +1,7 @@
 
-c() { cd ~/Prosjekter/$1; }
-_c() { _files -W ~/Prosjekter -/; }
-compdef _c c
+p() { cd ~/Prosjekter/$1; }
+_p() { _files -W ~/Prosjekter -/; }
+compdef _p p
 
 h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
