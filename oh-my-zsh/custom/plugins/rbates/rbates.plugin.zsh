a0ddbb20c7cada558ffb170fdcca11e8004187df
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-03-11 14:19:36 +0100

    Add projects http dir to autocomplete

--- oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
+++ oh-my-zsh/custom/plugins/rbates/rbates.plugin.zsh
@@ -3,6 +3,10 @@ p() { cd ~/Projects/$1; }
 _p() { _files -W ~/Projects -/; }
 compdef _p p
 
+ph() { cd ~/Projects/http/$1; }
+_ph() { _files -W ~/Projects/http -/; }
+compdef _ph ph
+
 h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
