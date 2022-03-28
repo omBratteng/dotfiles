0c69022f1778e0aa00f9e90428d3d100cac7085a
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2022-03-28 10:31:05 +0200

    fix: only install `n` on macOS and use brew formula

--- scripts/n.zsh
+++ scripts/n.zsh
@@ -8,33 +8,20 @@ function install() {
 	if command_exists -v n; then
 		false
 	else
-		_tmpdir=$(mktemp -d)
-		git clone --quiet https://github.com/tj/n.git $_tmpdir
-		cd $_tmpdir
-		sudo make install >/dev/null 2>&1
-		cd -
-		rm -rf $_tmpdir
-		unset _tmpdir
-		return
+		if [[ "$(uname)" == "Darwin" ]]; then
+			if command_exists -v brew; then
+				brew install n
+			else
+				echo "Homebrew doesn't exist"
+				false
+			fi
+		fi
 	fi
 }
 
 function upgrade() {
 	if command_exists -v n; then
-		_tmpdir=$(mktemp -d)
-		git clone --quiet https://github.com/tj/n.git $_tmpdir
-		cd $_tmpdir
-
-		_latest=$(git describe --abbrev=0 | sed -En 's/v(.*)/\1/p')
-		_current=$(n --version)
-
-		if [ ! $_latest = $_current ]; then
-			sudo make install >/dev/null 2>&1
-		fi
-
-		cd -
-		rm -rf $_tmpdir
-		unset _tmpdir _latest _current
+		# upgrade is handled by the package manager
 		return
 	else
 		install
