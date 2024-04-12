ace1faa348cd35a93d583b44ce290b345a1c35fa
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-04-12 20:55:51 +0200

    Support multi-arch smallstep install

--- scripts/step.zsh
+++ scripts/step.zsh
@@ -22,14 +22,18 @@ function install() {
 				_tmpdir=$(mktemp -d)
 				_step_latest_version=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 				_step_latest_version=${_step_latest_version:1}
+				_arch="amd64"
+				if [[ "$(uname -m)" == "aarch64" ]]; then
+					_arch="arm64"
+				fi
 
 				cd "$_tmpdir" || exit
 				if command_exists -v dpkg; then
-					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_amd64.deb"
-					sudo dpkg -i "step-cli_${_step_latest_version}_amd64.deb"
+					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_${_arch}.deb"
+					sudo dpkg -i "step-cli_${_step_latest_version}_${_arch}.deb"
 				elif command_exists -v dnf; then
-					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_amd64.rpm"
-					sudo dnf install "step-cli_${_step_latest_version}_amd64.rpm"
+					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_${_arch}.rpm"
+					sudo dnf install "step-cli_${_step_latest_version}_${_arch}.rpm"
 				else
 					echo "Could not find a valid package manager"
 					false
