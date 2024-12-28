f2cdc4582eb606588b2f4315c8f4f15aba2135ef
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-12-28 04:03:53 +0100

    fix: update step install for rhel

--- scripts/step.zsh
+++ scripts/step.zsh
@@ -22,9 +22,9 @@ function install() {
 				_tmpdir=$(mktemp -d)
 				_step_latest_version=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 				_step_latest_version=${_step_latest_version:1}
-				_arch="amd64"
+				_arch="x86_64"
 				if [[ "$(uname -m)" == "aarch64" ]]; then
-					_arch="arm64"
+					_arch="aarch64"
 				fi
 
 				# If _FORCE_INSTALL is set, add the -y flag to the package manager
@@ -34,11 +34,15 @@ function install() {
 
 				cd "$_tmpdir" || exit
 				if command_exists -v dpkg; then
-					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_${_arch}.deb"
-					sudo dpkg -i "step-cli_${_step_latest_version}_${_arch}.deb"
+					_arch="amd64"
+					if [[ "$(uname -m)" == "aarch64" ]]; then
+						_arch="arm64"
+					fi
+					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}-1_${_arch}.deb"
+					sudo dpkg -i "step-cli_${_step_latest_version}-1_${_arch}.deb"
 				elif command_exists -v dnf; then
-					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli_${_step_latest_version}_${_arch}.rpm"
-					sudo dnf install $_args "step-cli_${_step_latest_version}_${_arch}.rpm"
+					curl -LO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_latest_version}/step-cli-${_step_latest_version}-1.${_arch}.rpm"
+					sudo dnf install $_args "step-cli-${_step_latest_version}-1.${_arch}.rpm"
 				else
 					echo "Could not find a valid package manager"
 					false
@@ -63,6 +67,10 @@ function upgrade() {
 				_step_latest_version=$(curl -s https://api.github.com/repos/smallstep/cli/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 				_step_latest_version=${_step_latest_version:1}
 				_step_version=$(step version | grep -oP '(?<=Smallstep CLI/)(.*)(?= )')
+				_arch="x86_64"
+				if [[ "$(uname -m)" == "aarch64" ]]; then
+					_arch="aarch64"
+				fi
 				if [[ "${_step_latest_version}" == "$_step_version" ]]; then
 					return
 				else
@@ -73,11 +81,15 @@ function upgrade() {
 				cd "${_tmpdir}" || exit
 
 				if command_exists -v dpkg; then
-					curl -sLO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_version}/step-cli_${_step_version}_amd64.deb"
-					sudo dpkg -i "step-cli_${_step_version}_amd64.deb"
+					_arch="amd64"
+					if [[ "$(uname -m)" == "aarch64" ]]; then
+						_arch="arm64"
+					fi
+					curl -sLO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_version}/step-cli_${_step_version}-1_${_arch}.deb"
+					sudo dpkg -i "step-cli_${_step_version}-1_${_arch}.deb"
 				elif command_exists -v dnf; then
-					curl -sLO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_version}/step-cli_${_step_version}_amd64.rpm"
-					sudo dnf install "step-cli_${_step_version}_amd64.rpm"
+					curl -sLO "https://dl.smallstep.com/gh-release/cli/docs-ca-install/v${_step_version}/step-cli-${_step_version}-1.${_arch}.rpm"
+					sudo dnf install "step-cli-${_step_version}-1.${_arch}.rpm"
 				else
 					echo "Could not find a valid package manager"
 					false
