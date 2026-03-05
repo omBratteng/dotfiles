c9d3cbdc49acf477747e24db469b633b773af8a5
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-03-05 14:12:14 +0100

    refactor: allow dynamic `pd`

--- .oh-my-zsh/custom/plugins/projects/projects.plugin.zsh
+++ .oh-my-zsh/custom/plugins/projects/projects.plugin.zsh
@@ -6,7 +6,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	p() { cd ~/Developer/"$1"; }
 	_p() { _files -W ~/Developer -/; }
 	compdef _p p
-	pd() { cd ~/Developer/dailydotdev/"$1"; }
-	_pd() { _files -W ~/Developer/dailydotdev -/; }
+
+	pd() {
+		if [[ -z "$_PD_WORK_DIR" ]]; then
+			echo "Error: _PD_WORK_DIR is not set" >&2
+			return 1
+		fi
+		cd ~/Developer/"$_PD_WORK_DIR"/"$1"
+	}
+	_pd() {
+		if [[ -z "$_PD_WORK_DIR" ]]; then
+			compadd -x "Error: _PD_WORK_DIR is not set"
+			return 1
+		fi
+		_files -W ~/Developer/"$_PD_WORK_DIR" -/
+	}
 	compdef _pd pd
-fi;
+fi
