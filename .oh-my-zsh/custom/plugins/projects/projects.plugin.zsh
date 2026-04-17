ef0b9463652043c3d65e73a0c0af086374216b57
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-04-17 06:28:18 +0200

    feat: make projects plugin fully configurable

--- .oh-my-zsh/custom/plugins/projects/projects.plugin.zsh
+++ .oh-my-zsh/custom/plugins/projects/projects.plugin.zsh
@@ -1,25 +1,27 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	p() { cd /srv/"$1"; }
-	_p() { _files -W /srv -/; }
-	compdef _p p
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	p() { cd ~/Developer/"$1"; }
-	_p() { _files -W ~/Developer -/; }
-	compdef _p p
-
-	pd() {
-		if [[ -z "$_PD_WORK_DIR" ]]; then
-			echo "Error: _PD_WORK_DIR is not set" >&2
-			return 1
-		fi
-		cd ~/Developer/"$_PD_WORK_DIR"/"$1"
-	}
-	_pd() {
-		if [[ -z "$_PD_WORK_DIR" ]]; then
-			compadd -x "Error: _PD_WORK_DIR is not set"
-			return 1
-		fi
-		_files -W ~/Developer/"$_PD_WORK_DIR" -/
-	}
-	compdef _pd pd
+if [[ -z "$_PD_DIR" ]]; then
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		_PD_DIR=/srv
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		_PD_DIR=~/Developer
+	fi
 fi
+
+p() { cd "$_PD_DIR/$1"; }
+_p() { _files -W "$_PD_DIR" -/; }
+compdef _p p
+
+pd() {
+	if [[ -z "$_PD_WORK_DIR" ]]; then
+		echo "Error: _PD_WORK_DIR is not set" >&2
+		return 1
+	fi
+	cd "$_PD_DIR/$_PD_WORK_DIR/$1"
+}
+_pd() {
+	if [[ -z "$_PD_WORK_DIR" ]]; then
+		compadd -x "Error: _PD_WORK_DIR is not set"
+		return 1
+	fi
+	_files -W "$_PD_DIR/$_PD_WORK_DIR" -/
+}
+compdef _pd pd
